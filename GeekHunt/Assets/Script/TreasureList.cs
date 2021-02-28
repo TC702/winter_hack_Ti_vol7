e7143135d8c28955cdcf12030a878f8948b38c3b
TC702 <sas.taisei.1129@gmail.com>
2021-02-28 13:21:56 +0900

    fix few change

--- GeekHunt/Assets/Script/TreasureList.cs
+++ GeekHunt/Assets/Script/TreasureList.cs
@@ -7,28 +7,28 @@ public class TreasureList : MonoBehaviour
     private int size = 9;
     public string[] title = new string[9]
     {
-        "ひろゆき",
+        "Hiroyuki",
         "2ch",
         "掲示板",
         "SE",
-        "プログラマー",
-        "u",
-        "e",
+        "Programer",
+        "Program",
+        "OpenSource",
         "フルスタックエンジニア",
-        "k"
+        "Unity"
 
     };
     public string[] detail = new string[9]
     {
-        "2chを作った人",
-        "ネットの匿名掲示板",
-        "ゴミだめ",
+        "2chを作った人。写像については知らない。",
+        "ネットの匿名掲示板。テレビの２チャンネルで流していいものではない。",
+        "人の欲望が集まる場所。人類の叡智",
         "情報システム関連の業務に従事する者",
         "コンピュータのプログラムを作成する人",
-        "u",
-        "e",
-        "o",
-        "k"
+        "テキスト。人がコンピュータに命令する時の橋渡しな役割。",
+        "誰でも使えるソースコード/ソフトウェア。先人の遺産",
+        "強い",
+        "3Dゲーム作成によく用いられるフレームワーク。2Dゲームを作る際にも活躍。"
     };
     public bool[] isHave = new bool[9]
     {
@@ -38,10 +38,12 @@ public class TreasureList : MonoBehaviour
         false,
         true,
         false,
-        false,
+        true,
         true,
         false
     };
+
+    //public 
 
     public int Size { get => size; set => size = value; }
 
